b12e1231c61b2d571d834a7a525487f2de41fcb5
Menghui.Guan <menghui.guan@csdental.com>
2021-06-11 15:56:55 +0800

    improve: let Allocator inherit from std::allocator to make list<Allocator> support merge and splice operation.
    add more test cases.

--- Allocator.h
+++ Allocator.h
@@ -6,166 +6,149 @@
 namespace Moya {
 
 template <class T, std::size_t growSize = 1024>
-class MemoryPool
-{
-    struct Block
-    {
-        Block *next;
-    };
+class MemoryPool {
+  struct Block {
+    Block *next;
+  };
 
-    class Buffer
-    {
-        static const std::size_t blockSize = sizeof(T) > sizeof(Block) ? sizeof(T) : sizeof(Block);
-        uint8_t data[blockSize * growSize];
+  class Buffer {
+    static const std::size_t blockSize = sizeof(T) > sizeof(Block) ? sizeof(T) : sizeof(Block);
+    uint8_t data[blockSize * growSize];
 
-        public:
+   public:
 
-            Buffer *const next;
+    Buffer *const next;
 
-            Buffer(Buffer *next) :
-                next(next)
-            {
-            }
+    Buffer(Buffer *next) :
+      next(next) {
+    }
 
-            T *getBlock(std::size_t index)
-            {
-                return reinterpret_cast<T *>(&data[blockSize * index]);
-            }
-    };
+    T *getBlock(std::size_t index) {
+      return reinterpret_cast<T *>(&data[blockSize * index]);
+    }
+  };
 
-    Block *firstFreeBlock = nullptr;
-    Buffer *firstBuffer = nullptr;
-    std::size_t bufferedBlocks = growSize;
+  Block *firstFreeBlock = nullptr;
+  Buffer *firstBuffer = nullptr;
+  std::size_t bufferedBlocks = growSize;
 
 
-    public:
+ public:
 
-        MemoryPool() = default;
-        MemoryPool(MemoryPool &&memoryPool) = delete;
-        MemoryPool(const MemoryPool &memoryPool) = delete;
-        MemoryPool operator =(MemoryPool &&memoryPool) = delete;
-        MemoryPool operator =(const MemoryPool &memoryPool) = delete;
+  MemoryPool() = default;
+  MemoryPool(MemoryPool &&memoryPool) = delete;
+  MemoryPool(const MemoryPool &memoryPool) = delete;
+  MemoryPool operator =(MemoryPool &&memoryPool) = delete;
+  MemoryPool operator =(const MemoryPool &memoryPool) = delete;
 
-        ~MemoryPool()
-        {
-            while (firstBuffer) {
-                Buffer *buffer = firstBuffer;
-                firstBuffer = buffer->next;
-                delete buffer;
-            }
-        }
+  ~MemoryPool() {
+    while (firstBuffer) {
+      Buffer *buffer = firstBuffer;
+      firstBuffer = buffer->next;
+      delete buffer;
+    }
+  }
 
-        T *allocate()
-        {
-            if (firstFreeBlock) {
-                Block *block = firstFreeBlock;
-                firstFreeBlock = block->next;
-                return reinterpret_cast<T *>(block);
-            }
+  T *allocate() {
+    if (firstFreeBlock) {
+      Block *block = firstFreeBlock;
+      firstFreeBlock = block->next;
+      return reinterpret_cast<T *>(block);
+    }
 
-            if (bufferedBlocks >= growSize) {
-                firstBuffer = new Buffer(firstBuffer);
-                bufferedBlocks = 0;
-            }
+    if (bufferedBlocks >= growSize) {
+      firstBuffer = new Buffer(firstBuffer);
+      bufferedBlocks = 0;
+    }
 
-            return firstBuffer->getBlock(bufferedBlocks++);
-        }
+    return firstBuffer->getBlock(bufferedBlocks++);
+  }
 
-        void deallocate(T *pointer)
-        {
-            Block *block = reinterpret_cast<Block *>(pointer);
-            block->next = firstFreeBlock;
-            firstFreeBlock = block;
-        }
+  void deallocate(T *pointer) {
+    Block *block = reinterpret_cast<Block *>(pointer);
+    block->next = firstFreeBlock;
+    firstFreeBlock = block;
+  }
 };
 
 template <class T, std::size_t growSize = 1024>
-class Allocator : private MemoryPool<T, growSize>
-{
+class Allocator : public std::allocator<T>, private MemoryPool<T, growSize> {
 #ifdef _WIN32
-    Allocator *copyAllocator = nullptr;
-    std::allocator<T> *rebindAllocator = nullptr;
+  Allocator *copyAllocator = nullptr;
+  std::allocator<T> *rebindAllocator = nullptr;
 #endif
 
-    public:
+ public:
 
-        typedef std::size_t size_type;
-        typedef std::ptrdiff_t difference_type;
-        typedef T *pointer;
-        typedef const T *const_pointer;
-        typedef T &reference;
-        typedef const T &const_reference;
-        typedef T value_type;
+  //typedef std::size_t size_type;
+  //typedef std::ptrdiff_t difference_type;
+  //typedef T *pointer;
+  //typedef const T *const_pointer;
+  //typedef T &reference;
+  //typedef const T &const_reference;
+  //typedef T value_type;
 
-        template <class U>
-        struct rebind
-        {
-            typedef Allocator<U, growSize> other;
-        };
+  template <class U>
+  struct rebind {
+    typedef Allocator<U, growSize> other;
+  };
 
 #ifdef _WIN32
-        Allocator() = default;
+  Allocator() = default;
 
-        Allocator(Allocator &allocator) :
-            copyAllocator(&allocator)
-        {
-        }
+  Allocator(Allocator &allocator) :
+    copyAllocator(&allocator) {
+  }
 
-        template <class U>
-        Allocator(const Allocator<U, growSize> &other)
-        {
-            if (!std::is_same<T, U>::value)
-                rebindAllocator = new std::allocator<T>();
-        }
+  template <class U>
+  Allocator(const Allocator<U, growSize> &other) {
+    if (!std::is_same<T, U>::value)
+      rebindAllocator = new std::allocator<T>();
+  }
 
-        ~Allocator()
-        {
-            delete rebindAllocator;
-        }
+  ~Allocator() {
+    delete rebindAllocator;
+  }
 #endif
 
-        pointer allocate(size_type n, const void *hint = 0)
-        {
+  pointer allocate(size_type n, const void *hint = 0) {
 #ifdef _WIN32
-            if (copyAllocator)
-                return copyAllocator->allocate(n, hint);
+    if (copyAllocator)
+      return copyAllocator->allocate(n, hint);
 
-            if (rebindAllocator)
-                return rebindAllocator->allocate(n, hint);
+    if (rebindAllocator)
+      return rebindAllocator->allocate(n, hint);
 #endif
 
-            if (n != 1 || hint)
-                throw std::bad_alloc();
+    if (n != 1 || hint)
+      throw std::bad_alloc();
 
-            return MemoryPool<T, growSize>::allocate();
-        }
+    return MemoryPool<T, growSize>::allocate();
+  }
 
-        void deallocate(pointer p, size_type n)
-        {
+  void deallocate(pointer p, size_type n) {
 #ifdef _WIN32
-            if (copyAllocator) {
-                copyAllocator->deallocate(p, n);
-                return;
-            }
+    if (copyAllocator) {
+      copyAllocator->deallocate(p, n);
+      return;
+    }
 
-            if (rebindAllocator) {
-                rebindAllocator->deallocate(p, n);
-                return;
-            }
+    if (rebindAllocator) {
+      rebindAllocator->deallocate(p, n);
+      return;
+    }
 #endif
 
-            MemoryPool<T, growSize>::deallocate(p);
-        }
+    MemoryPool<T, growSize>::deallocate(p);
+  }
 
-        void construct(pointer p, const_reference val)
-        {
-            new (p) T(val);
-        }
+  void construct(pointer p, const_reference val) {
+    new (p) T(val);
+  }
 
-        void destroy(pointer p)
-        {
-            p->~T();
-        }
+  void destroy(pointer p) {
+    p->~T();
+  }
 };
 
 }
--- Measure.cpp
+++ Measure.cpp
@@ -8,141 +8,183 @@
 #include <forward_list>
 
 #include "Allocator.h"
-
+#include <iostream>
+#include <omp.h>
 const std::size_t growSize = 1024;
 const int numberOfIterations = 1024;
 const int randomRange = 1024;
 
 template <typename Container>
-class PerformanceTest
-{
-    virtual void testIteration(int newSize) = 0;
+class PerformanceTest {
+  virtual void testIteration(int newSize) = 0;
 
 
-    protected:
+ protected:
 
-        Container container;
+  Container container;
 
-        std::default_random_engine randomNumberGenerator;
-        std::uniform_int_distribution<int> randomNumberDistribution;
+  std::default_random_engine randomNumberGenerator;
+  std::uniform_int_distribution<int> randomNumberDistribution;
 
 
-    public:
+ public:
 
-        PerformanceTest() :
-            randomNumberGenerator(0),
-            randomNumberDistribution(0, randomRange)
-        {
-        }
+  PerformanceTest() :
+    randomNumberGenerator(0),
+    randomNumberDistribution(0, randomRange) {
+  }
 
-        double run()
-        {
-            auto from = std::chrono::high_resolution_clock::now();
+  double run() {
+    auto from = std::chrono::high_resolution_clock::now();
 
-            for (int i = 0; i < numberOfIterations; i++)
-                testIteration(randomNumberDistribution(randomNumberGenerator));
+    for (int i = 0; i < numberOfIterations; i++)
+      testIteration(randomNumberDistribution(randomNumberGenerator));
 
-            auto to = std::chrono::high_resolution_clock::now();
-            return std::chrono::duration_cast<std::chrono::duration<double>>(to - from).count();
-        }
+    auto to = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::duration<double>>(to - from).count();
+  }
 };
 
 template <typename Container>
-class PushFrontTest : public PerformanceTest<Container>
-{
-    virtual void testIteration(int newSize)
-    {
-        int size = 0;
+class PushFrontTest : public PerformanceTest<Container> {
+  virtual void testIteration(int newSize) {
+    int size = 0;
 
-        while (size < newSize)
-            this->container.push_front(size++);
+    while (size < newSize)
+      this->container.push_front(size++);
 
-        for (; size > newSize; size--)
-            this->container.pop_front();
-    }
+    for (; size > newSize; size--)
+      this->container.pop_front();
+  }
 };
 
 template <typename Container>
-class PushBackTest : public PerformanceTest<Container>
-{
-    virtual void testIteration(int newSize)
-    {
-        int size = 0;
+class PushBackTest : public PerformanceTest<Container> {
+  virtual void testIteration(int newSize) {
+    int size = 0;
 
-        while (size < newSize)
-            this->container.push_back(size++);
+    while (size < newSize)
+      this->container.push_back(size++);
 
-        for (; size > newSize; size--)
-            this->container.pop_back();
-    }
+    for (; size > newSize; size--)
+      this->container.pop_back();
+  }
 };
 
 template <typename Container>
-class MapTest : public PerformanceTest<Container>
-{
-    virtual void testIteration(int newSize)
-    {
-        int size = 0;
+class MapTest : public PerformanceTest<Container> {
+  virtual void testIteration(int newSize) {
+    int size = 0;
 
-        while (size < newSize)
-            this->container.insert(std::pair<char, int>(size++, size));
+    while (size < newSize)
+      this->container.insert(std::pair<char, int>(size++, size));
 
-        while (size > newSize)
-            this->container.erase(--size);
-    }
+    while (size > newSize)
+      this->container.erase(--size);
+  }
 };
 
 template <typename Container>
-class SetTest : public PerformanceTest<Container>
-{
-    virtual void testIteration(int newSize)
-    {
-        int size = 0;
+class SetTest : public PerformanceTest<Container> {
+  virtual void testIteration(int newSize) {
+    int size = 0;
 
-        while (size < newSize)
-            this->container.insert(size++);
+    while (size < newSize)
+      this->container.insert(size++);
 
-        while (size > newSize)
-            this->container.erase(--size);
-    }
+    while (size > newSize)
+      this->container.erase(--size);
+  }
 };
 
 template <typename StlContainer, typename FastContainer>
-void printTestStatus(const char *name, StlContainer &stlContainer, FastContainer &fastContainer)
-{
-    std::cout << std::fixed;
-    std::cout << name << " - Default STL Allocator : " << stlContainer.run() << " seconds." << std::endl;
-    std::cout << name << " - Memory Pool Allocator : " << fastContainer.run() << " seconds." << std::endl;
-    std::cout << std::endl;
+void printTestStatus(const char *name, StlContainer &stlContainer, FastContainer &fastContainer) {
+  std::cout << std::fixed;
+  std::cout << name << " - Default STL Allocator : " << stlContainer.run() << " seconds." << std::endl;
+  std::cout << name << " - Memory Pool Allocator : " << fastContainer.run() << " seconds." << std::endl;
+  std::cout << std::endl;
 }
 
-int main()
-{
-    typedef int DataType;
-    typedef Moya::Allocator<DataType, growSize> MemoryPoolAllocator;
+template<class Alloc>
+std::ostream &operator<<(std::ostream &ostr, std::list<int, Alloc> &list) {
+  for (auto &i : list) {
+    ostr << " " << i;
+  }
+  return ostr;
+}
+void sortTest() {
+  std::list<int, Moya::Allocator<int, 32>>l;
+  for (int i = 10; i > 0; i--) {
+    l.push_back(i);
+  }
+  std::cout << "before sort:" << l << "\n";
+  l.sort();
+  std::cout << "after sort :" << l << "\n";
+}
+void spiceTest() {
+  std::list<int, Moya::Allocator<int, 32>> list1 = { 1, 2, 3, 4, 5 };
+  std::list<int, Moya::Allocator<int, 32>> list2 = { 10, 20, 30, 40, 50 };
 
-    std::cout << "Allocator performance measurement example" << std::endl;
-    std::cout << "Version: 1.0" << std::endl << std::endl;
+  auto it = list1.begin();
+  std::advance(it, 2);
 
-    PushFrontTest<std::forward_list<DataType>> pushFrontForwardListTestStl;
-    PushFrontTest<std::forward_list<DataType, MemoryPoolAllocator>> pushFrontForwardListTestFast;
-    printTestStatus("ForwardList PushFront", pushFrontForwardListTestStl, pushFrontForwardListTestFast);
+  list1.splice(it, list2);
 
-    PushFrontTest<std::list<DataType>> pushFrontListTestStl;
-    PushFrontTest<std::list<DataType, MemoryPoolAllocator>> pushFrontListTestFast;
-    printTestStatus("List PushFront", pushFrontListTestStl, pushFrontListTestFast);
+  std::cout << "list1: " << list1 << "\n";
+  std::cout << "list2: " << list2 << "\n";
 
-    PushBackTest<std::list<DataType>> pushBackListTestStl;
-    PushBackTest<std::list<DataType, MemoryPoolAllocator>> pushBackListTestFast;
-    printTestStatus("List PushBack", pushBackListTestStl, pushBackListTestFast);
+  list2.splice(list2.begin(), list1, it, list1.end());
 
-    MapTest<std::map<DataType, DataType, std::less<DataType>>> mapTestStl;
-    MapTest<std::map<DataType, DataType, std::less<DataType>, MemoryPoolAllocator>> mapTestFast;
-    printTestStatus("Map", mapTestStl, mapTestFast);
+  std::cout << "list1: " << list1 << "\n";
+  std::cout << "list2: " << list2 << "\n";
+}
 
-    SetTest<std::set<DataType, std::less<DataType>>> setTestStl;
-    SetTest<std::set<DataType, std::less<DataType>, MemoryPoolAllocator>> setTestFast;
-    printTestStatus("Set", setTestStl, setTestFast);
+template<class Alloc>
+void pushbackTest(int n, char *str) {
+  double t0 = omp_get_wtime();
+  {
+    std::list<int, Alloc> l;
+    for (int i = 0; i < n; i++) {
+      l.push_back(i);
+    }
+  }
+  double t1 = omp_get_wtime();
+  printf("pusbackTest(%s) used time %fs\n", str, t1 - t0);
+}
 
-    return 0;
+int main() {
+  sortTest();
+  spiceTest();
+  int n = 1024;
+  pushbackTest<std::allocator<int>>(n, "std 1024");
+  pushbackTest<Moya::Allocator<int, 1024>>(n, "allocator 1024");
+  pushbackTest<std::allocator<int>>(n * n, "std 1024*1024");
+  pushbackTest<Moya::Allocator<int, 1024>>(n * n, "allocator 1024*1024");
+  typedef int DataType;
+  typedef Moya::Allocator<DataType, growSize> MemoryPoolAllocator;
+
+  std::cout << "Allocator performance measurement example" << std::endl;
+  std::cout << "Version: 1.0" << std::endl << std::endl;
+
+  PushFrontTest<std::forward_list<DataType>> pushFrontForwardListTestStl;
+  PushFrontTest<std::forward_list<DataType, MemoryPoolAllocator>> pushFrontForwardListTestFast;
+  printTestStatus("ForwardList PushFront", pushFrontForwardListTestStl, pushFrontForwardListTestFast);
+
+  PushFrontTest<std::list<DataType>> pushFrontListTestStl;
+  PushFrontTest<std::list<DataType, MemoryPoolAllocator>> pushFrontListTestFast;
+  printTestStatus("List PushFront", pushFrontListTestStl, pushFrontListTestFast);
+
+  PushBackTest<std::list<DataType>> pushBackListTestStl;
+  PushBackTest<std::list<DataType, MemoryPoolAllocator>> pushBackListTestFast;
+  printTestStatus("List PushBack", pushBackListTestStl, pushBackListTestFast);
+
+  MapTest<std::map<DataType, DataType, std::less<DataType>>> mapTestStl;
+  MapTest<std::map<DataType, DataType, std::less<DataType>, MemoryPoolAllocator>> mapTestFast;
+  printTestStatus("Map", mapTestStl, mapTestFast);
+
+  SetTest<std::set<DataType, std::less<DataType>>> setTestStl;
+  SetTest<std::set<DataType, std::less<DataType>, MemoryPoolAllocator>> setTestFast;
+  printTestStatus("Set", setTestStl, setTestFast);
+
+  return 0;
 }
